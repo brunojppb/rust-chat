64976415c0f9f31535ea50bb5ae4c21e4caa2a01
Bruno Paulino <bruno@bpaulino.com>
2023-02-05 23:31:50 +0100

    Use async closures to accept multiple connections

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,26 @@ use tokio::{
 };
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("localhost:8080").await?;
-    let (mut socket, _addr) = listener.accept().await?;
-
-    let (read, mut write) = socket.split();
-    let mut reader = BufReader::new(read);
-    let mut line = String::new();
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
     loop {
-        let bytes_read = reader.read_line(&mut line).await?;
-        if bytes_read == 0 {
-            break;
-        }
+        let (mut socket, _addr) = listener.accept().await.unwrap();
 
-        write.write_all(&line.as_bytes()).await?;
-        line.clear();
+        tokio::spawn(async move {
+            let (read, mut write) = socket.split();
+            let mut reader = BufReader::new(read);
+            let mut line = String::new();
+
+            loop {
+                let bytes_read = reader.read_line(&mut line).await.unwrap();
+                if bytes_read == 0 {
+                    break;
+                }
+
+                write.write_all(&line.as_bytes()).await.unwrap();
+                line.clear();
+            }
+        });
     }
-
-    Ok(())
 }
